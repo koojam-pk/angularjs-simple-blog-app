32f601e106e3e33a652e3cdf7f960fe8eef112a5
koojam-pk <pakkian.lew@pakkianlew.com>
2017-09-26 13:11:06 +0800

    Improve UI

--- web.js
+++ web.js
@@ -1,14 +1,30 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var morgan = require('morgan');
+// var bodyParser = require('body-parser');
+// var methodOverride = require('method-override');
 var app = express();
 
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Request-Method', '*');
+  res.header('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.header('Access-Control-Allow-Headers', 'authorization, content-type');
+  next();
+});
+
+// app.use(bodyParser.urlencoded({'extended': 'true'}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.json({'type': 'application/vnd.api+json'}));
+
+// app.use(methodOverride());
+
 app.get('*', (req, res) => {
   res.writeHead(301, {
-    Location: "http" + (req.socket.encrypted ? "s" : "") + "://" + req.headers.host + '/#' + req.url
+    Location: "http" + (req.connection.encrypted ? "s" : "") + "://" + req.headers.host + '/#' + req.url
   });
   res.end();
 });
